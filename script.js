d0d31c0a7e54bd3cc7b0d6bbfaeaa0584b04ea9b
Mohammed Falih <yunusfalih123@gmail.com>
2024-01-11 08:04:41 +0530

    final update

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 const questions = [
     {
-        question: "what is your name?" ,
+        question: "Which of the following is used to create web pages??" ,
         answers: [
-            {text:"Thoufik", correct: false},
-            {text:"Falih", correct: true},
-            {text:"Sulthan", correct: false},
-            {text:"Apsar", correct: false},
+            {text:"C", correct: false},
+            {text:"HTML", correct: true},
+            {text:"Python", correct: false},
+            {text:"C++", correct: false},
         ]
     },
     {
-        question: "what is your name?" ,
+        question: "Opening tag of HTML is called?" ,
         answers: [
-            {text:"Thoufik", correct: false},
-            {text:"Falih", correct: true},
-            {text:"Sulthan", correct: false},
-            {text:"Apsar", correct: false},
+            {text:"Pair tag", correct: false},
+            {text:"Closed tag", correct: false},
+            {text:"Ending tag", correct: false},
+            {text:"Starting tag", correct: true},
         ]
     },
     {
-        question: "what is your name?" ,
+        question: "Which is the largest heading tag??" ,
         answers: [
-            {text:"Thoufik", correct: false},
-            {text:"Falih", correct: true},
-            {text:"Sulthan", correct: false},
-            {text:"Apsar", correct: false},
+            {text:"H3", correct: false},
+            {text:"H1", correct: true},
+            {text:"H6", correct: false},
+            {text:"H4", correct: false},
+        ]
+    },
+    {
+        question: " What is the extension of javascript file?",
+        answers: [
+            {text:".js", correct:true},
+            {text:".java", correct:false},
+            {text:".jp", correct:false},
+            {text:".json", correct:false}
         ]
     }
 ];
@@ -73,10 +82,45 @@ function selectAnswer(e)
     const isCorrect = selectedBtn.dataset.correct === "true";
     if(isCorrect) {
         selectedBtn.classList.add("correct");
+        score++;
     }
     else {
         selectedBtn.classList.add("incorrect");
     }
+
+    Array.from(answerButtons.children).forEach( button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct")
+        }
+        button.disabled = true;
+    })
+    next.style.display = "block";
 }
+
+function showScore() {
+    reset();
+    questionElement.innerHTML = "You have Scored " + score;
+    next.innerHTML = "Restart";
+    next.style.display = "block";
+}
+
+function handleNext() {
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length) {
+        showQuestion();
+    }
+    else {
+        showScore();
+    }
+}
+
+next.addEventListener("click",() => {
+    if(currentQuestionIndex < questions.length){
+        handleNext();
+    }
+    else {
+        quizStart();
+    }
+})
 
 quizStart();
